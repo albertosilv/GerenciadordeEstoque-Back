8f62ee0c77cb8fe0d4e4ccbbff751cf0c703dc7e
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-04-26 20:57:52 -0300

    feature: upload images to Clodinary CDN

--- src/config/multer.js
+++ src/config/multer.js
@@ -5,13 +5,11 @@ const crypto = require('crypto');
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
-    },
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err);
-
+        if (err) {
+          cb(err);
+        }
         const fileName = `${hash.toString('HEX')}-${file.originalname}`;
         cb(null, fileName);
       });
